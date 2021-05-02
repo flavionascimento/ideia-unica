f5585078a3de7dfca3af03dc3d404815e8543768
Flavio <flavionascimento@MacBook-Air-de-Flavio.local>
2021-05-02 15:50:30 -0300

    Segunda Pagina

---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import Link from 'next/Link';
+function Sobre() {
+
+    return <div>
+        <h1>Home</h1>
+         <Link href="/index">
+           <a>Acessar página Home</a>
+          </Link>         
+        
+        </div>
+}
+
+export default Home
